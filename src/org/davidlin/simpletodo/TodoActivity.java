ac7b06a542e6bd9c7705c52c3be2ee96f2f513d8
David Lin <linwdav@gmail.com>
2013-09-20 22:29:17 -0700

    First commit

---
+++ src/org/davidlin/simpletodo/TodoActivity.java
@@ -0,0 +1,90 @@
+package org.davidlin.simpletodo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+/**
+ * Simple to-do list app.
+ * @author David
+ */
+public class TodoActivity extends Activity {
+
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView lvItems;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo);
+		readItems();  // Load saved list of items into ArrayList
+		lvItems = (ListView) findViewById(R.id.lvItems);
+		itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+		lvItems.setAdapter(itemsAdapter);
+		setupListViewListener();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.todo, menu);
+		return true;
+	}
+
+	private void setupListViewListener() {
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> aView, View item,
+					int pos, long id) {
+				items.remove(pos);
+				itemsAdapter.notifyDataSetInvalidated();
+				saveItems();
+				return true;
+			}
+		});
+	}
+	
+	public void addTodoItem(View v) {
+		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+		String etNewItemText = etNewItem.getText().toString();
+		if (!etNewItemText.isEmpty() && !etNewItemText.trim().isEmpty()) {
+			itemsAdapter.add(etNewItemText);
+		}
+		etNewItem.setText("");
+	}
+	
+	private void readItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			items = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException e) {
+			items = new ArrayList<String>();
+			e.printStackTrace();
+		}
+	}
+	
+	private void saveItems() {
+		File filesDir = getFilesDir();
+		File todoFile = new File(filesDir, "todo.txt");
+		try {
+			FileUtils.writeLines(todoFile,  items);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
